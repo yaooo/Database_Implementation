31b77c0532a61cf8b40e5ce22db5d1c6d142de1b
yaooo <yaoshi616@gmail.com>
2020-02-10 15:08:30 -0500

    push little changes

---
+++ BufMgr-bin/CMakeFiles/minibase-bufmgr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minibase-bufmgr.dir/main.o"
+  "CMakeFiles/minibase-bufmgr.dir/test.o"
+  "minibase-bufmgr.pdb"
+  "minibase-bufmgr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minibase-bufmgr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
